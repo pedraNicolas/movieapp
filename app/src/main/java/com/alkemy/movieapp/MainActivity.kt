4b3fad7aca9504ef930a787015a32aa13b67fc86
pedra <pedranicolas>
2022-05-28 18:08:08 -0300

    MovieApiService

---
+++ app/src/main/java/com/alkemy/movieapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.alkemy.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alkemy.movieapp.models.Movie
+import com.alkemy.movieapp.models.MovieResponse
+import com.alkemy.movieapp.services.MovieApiInterface
+import com.alkemy.movieapp.services.MovieApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun getMovieData(callback: (List<Movie>) -> Unit){
+        val apiService = MovieApiService.getInstance().create(MovieApiInterface:: class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieResponse>{
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+}
