4bc6fcb2df7a825b84709587c52c08237e7a4695
pedra <pedranicolas>
2022-05-31 21:24:36 -0300

    Lista desplegable

--- app/src/main/java/com/alkemy/movieapp/services/MovieApiInterface.kt
+++ app/src/main/java/com/alkemy/movieapp/services/MovieApiInterface.kt
@@ -6,9 +6,8 @@ import com.alkemy.movieapp.models.MovieResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
-val get_url:String ="/3/movie/$item2?api_key=bb259118f01c72338a5e8a5dc72d7701"
 interface MovieApiInterface {
-    @GET(get_url)
+    @GET("/3/movie/$item2?api_key=bb259118f01c72338a5e8a5dc72d7701")
     fun getMovieList(): Call<MovieResponse>
 
 }
